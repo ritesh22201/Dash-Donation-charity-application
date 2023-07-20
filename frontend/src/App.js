ea827ff627b16b2f6ab26ed47974a245ba0d2b21
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-07-20 15:44:42 +0530

    admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,18 @@ import {
 } from '@chakra-ui/react';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import AdminSideBar from './components/AdminSideBar';
+import AdminPage from './pages/AdminPage';
+
+
 
 
 function App() {
   return (
   //  <LoginPage/>
-  <SignupPage/>
+  // <SignupPage/>
+  //  <AdminPage/>
+  <AdminSideBar/>
   );
 }
 
