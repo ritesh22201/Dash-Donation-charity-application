7feaefae4f8bde423c823c08c9c2fd982992a15f
ritesh22201 <riteshgoswami22201@gmail.com>
2023-07-22 20:43:03 +0530

    code added

--- frontend/src/routes/AllRoutes.jsx
+++ frontend/src/routes/AllRoutes.jsx
@@ -7,18 +7,26 @@ import AdminSideBar from '../components/AdminSideBar';
 import Home from '../pages/Home';
 import Donation from '../pages/Donation'
 import Home from '../pages/Home'
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import SignupPage from '../pages/SignupPage'
 import LoginPage from '../pages/LoginPage'
+import Organization from '../pages/Organization';
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>} />
-  <Route path="/adminlogin" element={<AdminLogin />} />
-      <Route path="/admin" element={<AdminPage/>} />
-      <Route path="/donation" element={<Donation/>} />
-      <Route path="/users/register" element={<SignupPage/>} />
-      <Route path="/users/login" element={<LoginPage/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/adminlogin" element={<AdminLogin />} />
+      <Route path="/admin" element={<AdminPage />} />
+      <Route path="/users/register" element={<SignupPage />} />
+      <Route path='/organization' element={<Organization />} />
+      <Route path="/users/login" element={<LoginPage />} />
+      <Route path='/donation' element={<PrivateRoutes>
+        <Donation />
+      </PrivateRoutes>} />
+      <Route path='/donationdata' element={<PrivateRoutes>
+        <DonationList />
+      </PrivateRoutes>}>
+      </Route>
     </Routes>
   )
 }
